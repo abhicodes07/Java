94bc8fcbd50cfcae0e70ffebb6d96f7f1beb99b0
Abhinav <abhiasthana76@gmail.com>
2024-09-30 11:03:52 +0530

    Command line arguments

---
+++ Introduction/CmlineArgs.java
@@ -0,0 +1,14 @@
+
+// to test it out
+// run: java CmlineArgs.java python lua
+
+class CmlineArgs {
+  public static void main(String[] args) {
+    String obj_1;
+    String obj_2;
+    obj_1 = args[0];
+    obj_2 = args[1];
+    System.out.println("First Argument: " + obj_1);
+    System.out.println("Second Argument: " + obj_2);
+  }
+}
