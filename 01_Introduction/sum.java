b05f3ac9e0796e34bfab7934241b0eccc1f8f49c
Abhinav <abhiasthana76@gmail.com>
2024-10-11 23:23:26 +0530

    sum numbers

---
+++ 01_Introduction/sum.java
@@ -0,0 +1,11 @@
+public class sum {
+  public static void main(String[] args) {
+    int n1;
+    int n2;
+    int sum;
+    n1 = 20;
+    n2 = 40;
+    sum = n1 + n2;
+    System.out.println("Sum: " + sum);
+  }
+}
