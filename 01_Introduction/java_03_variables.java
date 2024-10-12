4262fbaa7729e791fb623b011fd680d0d3e68e96
Abhinav <abhiasthana76@gmail.com>
2024-10-12 16:40:36 +0530

    Variables and literals

---
+++ 01_Introduction/java_03_variables.java
@@ -0,0 +1,23 @@
+public class java_03_variables {
+  public static void main(String[] args) {
+    int num;
+    String s;
+    float f;
+    char c;
+    double d;
+    long l;
+    boolean b;
+    num = 2;
+    s = "Hii, there!";
+    f = 2.2f;
+    d = 2.23;
+    b = true;
+    c = 'A';
+    System.out.println(c);
+    System.out.println(num);
+    System.out.println(s);
+    System.out.println(f);
+    System.out.println(d);
+    System.out.println(b);
+  }
+}
