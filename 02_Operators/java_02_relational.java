99696a775dffe2f78b4e4f849ce3aa929c48697c
Abhinav <abhiasthana76@gmail.com>
2024-10-23 22:49:31 +0530

    Relational operator

---
+++ 02_Operators/java_02_relational.java
@@ -0,0 +1,13 @@
+public class java_02_relational {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("a < b: " + (a<b));
+        System.out.println("a > b: " + (a>b));
+        System.out.println("a <= b: " + (a<=b));
+        System.out.println("a >= b: " + (a>=b));
+        System.out.println("a == b: " + (a==b));
+    }
+}
