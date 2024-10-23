6a79aeab46a2fd1f4d9b2a6b6a563dfe99435547
Abhinav <abhiasthana76@gmail.com>
2024-10-23 22:50:44 +0530

    Increment & Decrement operator

---
+++ 02_Operators/java_05_inc_dec.java
@@ -0,0 +1,11 @@
+public class java_05_inc_dec {
+    public static void main(String[] args) {
+        int m = 10, n = 20;
+        System.out.println("m = " + m);
+        System.out.println("n = " + n);
+        System.out.println("++m: " + ++m);
+        System.out.println("n++: " + n++);
+        System.out.println("m: " + m);
+        System.out.println("n: " + n);
+    }
+}
