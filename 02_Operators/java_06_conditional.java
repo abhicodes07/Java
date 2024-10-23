71bd21f85d4284aed3eeb7eee96859da7a5307ea
Abhinav <abhiasthana76@gmail.com>
2024-10-23 22:51:09 +0530

    Conditional Operator

---
+++ 02_Operators/java_06_conditional.java
@@ -0,0 +1,9 @@
+public class java_06_conditional {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        String value;
+        value = a>b ? "a is greater than b." : "a is smaller than b.";
+        System.out.println("Value: " + value);
+    }
+}
