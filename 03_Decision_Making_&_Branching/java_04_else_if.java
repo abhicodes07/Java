9da89baca6d6ad96abf1e44e0aa2172875bb93fe
Abhinav <abhiasthana76@gmail.com>
2024-10-24 23:12:43 +0530

    else if

---
+++ 03_Decision_Making_&_Branching/java_04_else_if.java
@@ -0,0 +1,14 @@
+public class java_04_else_if {
+    public static void main(String[] args) {
+        int a = 202, b = 412, c = 809;
+        if (a > b) {
+            System.out.println(a);
+        }
+        else if (b > c) {
+            System.out.println(b);
+        }
+        else {
+            System.out.println(c);
+        }
+    }
+}
