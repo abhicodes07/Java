c82cb49eda2d2c2bf42c089fc6d2bae9535d2f58
Abhinav <abhiasthana76@gmail.com>
2024-10-29 17:47:52 +0530

    While Loop

--- 04_Looping/java_02_while.java
+++ 04_Looping/java_02_while.java
@@ -1,18 +1,10 @@
 import java.util.Scanner;
 public class java_02_while {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = 0, y = 0;
-        int rows;
-        System.out.print("Enter the number of rows: ");
-        rows = sc.nextInt();
-        while (x < rows) {
-            System.out.println();
-            while (y < x) {
-                System.out.print("* ");
-                y++;
-            }
-            x++;
+        int i = 1;
+        while (i<=10) {
+            System.out.println(i);
+            i++;
         }
     }
 }
