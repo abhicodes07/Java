8065ac450cb81d10cc25dd5668950b6c917b75de
Abhinav <abhiasthana76@gmail.com>
2024-10-29 17:49:11 +0530

    Break

---
+++ 04_Looping/java_07_break.java
@@ -0,0 +1,10 @@
+public class java_07_break {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+            if (i == 10) {
+                break;
+            }
+        }
+    }
+}
