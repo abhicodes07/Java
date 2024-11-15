a311c1341b420a2ba535ca341fc805e4bbcad425
Abhinav <abhiasthana76@gmail.com>
2024-11-15 20:46:37 +0530

    Vector Methods

---
+++ 07_Vectors/java_02_vector_methods.java
@@ -0,0 +1,12 @@
+import java.util.Vector;
+public class java_02_vector_methods {
+    public static void main(String[] args) {
+        Vector data = new Vector();
+        data.add(2);
+        data.add("Hii");
+        data.add(23.3);
+        data.add(true);
+
+        System.out.println(data);
+    }
+}3
