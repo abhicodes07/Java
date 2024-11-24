4c3f906d4c49bbf9d404c53a29368c47289b79a4
Abhinav <abhiasthana76@gmail.com>
2024-11-24 16:17:53 +0530

    Methods in java

---
+++ Object Oriented Programming/01_Methods/java_01_methods.java
@@ -0,0 +1,13 @@
+public class java_01_methods {
+    static int Max(int x, int y) {
+        if (x > y) {
+            return 1;
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println(Max(12, 45));
+        System.out.println(Max(1000, 45));
+        System.out.println(Max(70, 23));
+    }
+}
